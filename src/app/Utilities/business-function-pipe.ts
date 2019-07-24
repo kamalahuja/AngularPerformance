ff3f30b5aef15c69033c74836e40e9eee5e7467b
Kamal Ahuja <nitk.kamalahuja@gmail.com>
2019-07-23 22:32:57 -0400

    using memoization for caching the function

--- src/app/Utilities/business-function-pipe.ts
+++ src/app/Utilities/business-function-pipe.ts
@@ -1,18 +1,22 @@
 import {Pipe, PipeTransform} from '@angular/core';
-const fibonacci = (num: number): number => {
-    
+import * as memoize from 'lodash.memoize';
+
+//const memoize = require('lodash.memoize');
+
+const fibonacci = memoize((num: number): number => {
+  console.log("fibonacci called n times->" +num);
     if (num === 1 || num === 2) {
       return 1;
     }
     return fibonacci(num - 1) + fibonacci(num - 2);
-  };
+  });
 @Pipe({
     name : 'businessFunctionPipe',
     pure : true
 })
 export class BusinessFunctionPipe implements PipeTransform{
     transform(value : number) : number {
-      console.log("fibonacci called n times->" +value);
+      console.log("BusinessFunctionPipe->" +value);
         return fibonacci(value);
     }
  
